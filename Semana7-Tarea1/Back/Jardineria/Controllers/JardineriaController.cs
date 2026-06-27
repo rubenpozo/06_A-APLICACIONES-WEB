6c3784d94592a67ae69c568f1b3d067f68c0d519
Rubén Pozo <ruben_oso28@hotmail.com>
2026-06-27 16:37:11 -0500

    Semana7Tarea1: de reporte PDF

---
+++ Semana7-Tarea1/Back/Jardineria/Controllers/JardineriaController.cs
@@ -0,0 +1,27 @@
+﻿using Jardineria.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jardineria.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JardineriaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public JardineriaController(AppDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Jardineria.Models.Jardineria>>> Get() =>
+            await _context.Jardinerias.ToListAsync();
+
+        [HttpPost]
+        public async Task<ActionResult<Jardineria.Models.Jardineria>> Post(Jardineria.Models.Jardineria item)
+        {
+            _context.Jardinerias.Add(item);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = item.Id }, item);
+        }
+    }
+}
